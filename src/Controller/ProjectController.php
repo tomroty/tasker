1be24d25308f392d487cf78544cdee5ee9283f88
tomroty <tom.roty@proton.me>
2025-04-20 00:46:19 +0200

    ongoing project list

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,18 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProjectController extends AbstractController
 {
     #[Route('/project/{keyCode}', name: 'project_show')]
-    public function index(?Project $project): Response
+    public function show(?Project $project): Response
     {
-        return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+        return $this->render('project/show.html.twig', [
+            'project' => $project
         ]);
     }
 
     #[Route('/projects', name: 'project_list')]
     public function list(): Response
     {
-        return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+        return $this->render('project/list.html.twig', [
         ]);
     }
 }
